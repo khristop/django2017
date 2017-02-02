02636c7668d5a87064efea7725b0702a44f872c1
christopher <kevinchristo_garcia@hotmail.com>
2017-02-02 10:39:17 +0000

    inicio del curso, semana 1, juevez static files

--- apps/app1/models.py
+++ apps/app1/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    nombre = models.CharField(max_length=100)
+    autor = models.CharField(max_length=20)
+    año = models.IntegerField(default=2000)
+    fechaDeAdquisicion = models.DateField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.nombre
+
+
