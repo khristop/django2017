727a8b4ce2b11598c8bc80ffb8bf65a045a5e731
christopher <kevinchristo_garcia@hotmail.com>
2017-02-02 10:29:03 +0000

    inicio del curso, semana 1, juevez static files

--- apps/app1/views.py
+++ apps/app1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'base.html', {"titulo":"inicio"})
+    return render(request, 'base.html', {"titulo":"inicio", "valor":22})
 
 def principal( request):
     return render(request, 'inicio.html',{"titulo":"principal"})
